315e6d6e5c32b855e525df3e8a9cd6f9f71d575d
sapphx <sapphx@gmail.com>
2014-11-04 21:23:04 +0100

    Added AI customization at the begining of the game :)

--- Assets/Jenga_Game.cs
+++ Assets/Jenga_Game.cs
@@ -4,7 +4,16 @@ using System.Collections.Specialized;
 using UnityEngine;
 using System.Collections;
 
+
+
 public class Jenga_Game : MonoBehaviour {
+
+	public enum AIMode {
+		random = 0,
+		heurystic,
+		other, 
+		Length
+	}
 
 	public class Player {
 		public int id;
@@ -20,6 +29,8 @@ public class Jenga_Game : MonoBehaviour {
 
 	public Jenga_Control_Block jcb = null;
 	public Jenga_Controller jc = null;
+
+	public List<AIMode> AIModes;
 
 	private string	strPlayer = "", 
 					strPlayerBegin_Human = "Player ",
@@ -41,6 +52,7 @@ public class Jenga_Game : MonoBehaviour {
 		jc.canMove = true;
 
 		Players = new List<Player> ();
+		AIModes = new List<AIMode> ();
 
 		for (int i = 0; i < PlayerNumber; ++i)
 		{
@@ -82,14 +94,36 @@ public class Jenga_Game : MonoBehaviour {
 			strPlayer = (PlayerRace(jc.moveIterator % PlayerNumber)) + (jc.moveIterator % PlayerNumber).ToString ();
 		}
 
-
 	}
 
 	void OnGUI() {
 
 		if (editingEnded == false) 
 		{
-			GUI.Box (new Rect (10,10,140,190), "Preparing game");
+
+			if ( AIModes.ToArray().Length < ComputerPlayers ) {
+				for (int i = AIModes.ToArray().Length; i < ComputerPlayers; ++i)
+				{
+					AIModes.Add(new AIMode());
+				}
+			}
+
+			if ( AIModes.ToArray().Length > ComputerPlayers ) {
+				for (int i = AIModes.ToArray().Length; i > ComputerPlayers; --i)
+				{
+					AIModes.RemoveAt(i-1);
+				}
+			}
+
+			int oversize = 0;
+			int oversizeDelta = 25;
+
+			if ( ComputerPlayers > 0 )
+			{
+				oversize += 10 + oversizeDelta * ComputerPlayers;
+			}
+
+			GUI.Box (new Rect (10,10,140,190 + oversize), "Preparing game");
 
 			#region PlatyerNumber
 
@@ -110,20 +144,54 @@ public class Jenga_Game : MonoBehaviour {
 			GUI.Box ( new Rect (15,95,130,50), "Number of AI" );
 			GUI.TextField (new Rect (20, 120, 40, 20), ComputerPlayers.ToString());
 			
-			Debug.Log (strPlayerNumber);
+			//Debug.Log (strPlayerNumber);
 			if (GUI.Button (new Rect (65, 120, 30, 20), "-"))
 			{
-				--ComputerPlayers;
+				if (ComputerPlayers > 0) 
+				{
+					--ComputerPlayers;
+				}
 			}
 			if (GUI.Button (new Rect (100, 120, 30, 20), "+"))
 			{
-				++ComputerPlayers;
+				if (ComputerPlayers < PlayerNumber) 
+				{
+					++ComputerPlayers;
+				}
 			}
 			#endregion
 
-			if (GUI.Button (new Rect (20, 160, 120, 20), "Start Game"))
+			int sY = 160;
+
+			for (int i = 0; i < ComputerPlayers; ++i) 
+			{
+				GUI.TextField (new Rect (45, sY + i * oversizeDelta, 70, 20), AIModes[i].ToString());
+
+				if (GUI.Button (new Rect (20, sY + i * oversizeDelta, 20, 20), "<"))
+				{
+					if (AIModes[i] > 0) 
+					{
+						AIModes[i] -= 1;
+					}
+				}
+
+				if (GUI.Button (new Rect (120, sY + i * oversizeDelta, 20, 20), ">"))
+				{
+					if (AIModes[i] < AIMode.Length - 1) 
+					{
+						AIModes[i] += 1;
+					}
+				}
+			}
+
+			if (GUI.Button (new Rect (20, 160 + oversize, 120, 20), "Start Game"))
 			{
 				editingEnded = true;
+				string str = "";
+				foreach ( var i in AIModes ){
+					str += i.ToString() + " # ";
+				}
+				Debug.Log (str);
 			}
 		}
 		else
